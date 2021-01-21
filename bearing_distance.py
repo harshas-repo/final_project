abeda0759c3d59bdef65f3ed129b3c990ee508e0
Harshavardhan b <harsha00159@gmail.com>
2021-01-21 22:59:03 +0530

    updated bearing with improved end location output

--- bearing_distance.py
+++ bearing_distance.py
@@ -5,40 +5,59 @@
 import math
 
 # to caliculate bearing based on the current and destination location co-ordinates
+# updated version
 
 
 def bearing():
-    lat1 = math.radians(13.026101239125405)
-    long1 = math.radians(80.01504296085217)
-    # 13.03578525069192,
-    # apollo college co-ordinates
-    # lat2 = math.radians(13.036902955382336)
-    # long2 = math.radians(80.04622399073887)
-    # 13.029671532500425, 80.02968502821973
-    # 13.035804392956667, 80.0462244773235
-    lat2 = math.radians(13.035804392956667)
-    long2 = math.radians(80.0462244773235)
-    longdiff = math.radians((long2 - long1))
-    x = math.sin(longdiff) * math.cos(lat2)
-    y = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * \
-        math.cos(lat2) * math.cos(longdiff)
-    return math.degrees(math.atan2(x, y))
+    startLat = math.radians(13.026101239125405)
+    startLong = math.radians(80.01504296085217)
+    endLat = math.radians(13.035804392956667)
+    endLong = math.radians(80.0462244773235)
+    dLong = endLong - startLong
+    dPhi = math.log(math.tan(endLat/2.0+math.pi/4.0) /
+                    math.tan(startLat/2.0+math.pi/4.0))
+    if abs(dLong) > math.pi:
+        if dLong > 0.0:
+            dLong = -(2.0 * math.pi - dLong)
+    else:
+        dLong = (2.0 * math.pi + dLong)
+
+    bearing = (math.degrees(math.atan2(dLong, dPhi)) + 360.0) % 360.0
+    return bearing
+
+# def bearing():
+# lat1 = math.radians(13.026101239125405)
+# long1 = math.radians(80.01504296085217)
+# # 13.03578525069192,
+# # apollo college co-ordinates
+# # lat2 = math.radians(13.036902955382336)
+# # long2 = math.radians(80.04622399073887)
+# # 13.029671532500425, 80.02968502821973
+# # 13.035804392956667, 80.0462244773235
+# lat2 = math.radians(13.035804392956667)
+# long2 = math.radians(80.0462244773235)
+# longdiff = math.radians((long2 - long1))
+# x = math.sin(longdiff) * math.cos(lat2)
+# y = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * \
+# math.cos(lat2) * math.cos(longdiff)
+# return math.degrees(math.atan2(x, y))
 
 
-def disatance():
+def distance():
     R = 6371000
     lat1 = math.radians(13.026101239125405)
     long1 = math.radians(80.01504296085217)
-    radbear = math.radians(bearing())
-    distance = 6000 / 2  # abs(actual_distance - 6000)
+    bear = bearing()
+    radbear = math.radians(72)
+    print(f"bearing {bear}")
+    distance = 3254  # abs(actual_distance - 6000)
     lat2 = math.asin(math.sin(lat1)*math.cos(distance / R) +
                      math.cos(lat1)*math.sin(distance/R)*math.cos(radbear))
     lon2 = long1 + math.atan2(math.sin(radbear)*math.sin(distance / R)*math.cos(lat1),
                               math.cos(distance/R)-math.sin(lat1)*math.sin(lat2))
-
     print(math.degrees(lat2), math.degrees(lon2))
     return
 
 
 print(bearing())
-disatance()
+distance()
--- main.py
+++ main.py
@@ -20,3 +20,37 @@ class location:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
+
+
+def distBtw(lat1, lon1, lat2, lon2):
+    earthRadiusKm = 6371
+
+    dLat = math.radians(lat2-lat1)
+    dLon = math.radians(lon2-lon1)
+
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * \
+        math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    return earthRadiusKm * c
+
+
+# def distBtw(lat1, lng1, lat2, lng2):
+    # earthRadius = 3958.75
+    # dLat = math.radians(lat2-lat1)
+    # dLng = math.radians(lng2-lng1)
+    # a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(math.radians(lat1)) * \
+    # math.cos(math.radians(lat2)) * math.sin(dLng/2) * math.sin(dLng/2)
+    # c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    # dist = earthRadius * c
+    # return dist
+
+
+loc1 = location(13.026101239125405,
+                80.01504296085217)
+loc2 = location(13.035804392956667,
+                80.0462244773235)
+print(loc1.lat, loc1.lon, loc2.lat, loc2.lon)
+print(distBtw(loc1.lat, loc1.lon, loc2.lat, loc2.lon))
