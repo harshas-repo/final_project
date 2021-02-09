6d35cb19a24f97113dafc9175a52039728e6ac4b
Harshavardhan b <harsha00159@gmail.com>
2021-02-09 09:26:05 +0530

    Message format redesign ,base36 encoding tests and project status, plan, timeline and TODO org file are added

---
+++ LoRaEnv.py
@@ -0,0 +1,6 @@
+# if(loRaSF == 7) time = 7.808;
+# if(loRaSF == 8) time = 13.9776;
+# if(loRaSF == 9) time = 24.6784;
+# if(loRaSF == 10) time = 49.3568;
+# if(loRaSF == 11) time = 85.6064;
+# if(loRaSF == 12) time = 171.2128;
---
+++ msgformat.py
@@ -0,0 +1,35 @@
+from datetime import datetime as dt
+from baseconv import base36 as bs
+
+
+def fordat():
+    date = str(dt.now().date())
+    date = date.split('-')
+    date.reverse()
+    date = ''.join(date[:2]) + date[2][2:]
+    time = str(dt.now().time())
+    time = time.split(':')
+    time = ''.join(time[:2]) + time[2][:2]
+    print(date)
+    print(time)
+    return date+time
+
+
+temp = fordat()
+print(len(temp))
+print(bs.encode(temp))
+print(len(bs.encode(temp)))
+li = "2001:0db8:85a3:0000:0000:8a2e:0370:7334".split(':')
+binli = []
+for i in li:
+    binli.append(bs.encode(int(i, 16)))
+print(binli)
+print(':'.join(binli))
+print(len(':'.join(binli)))
+worst_case = []
+for i in range(8):
+    worst_case.append(bs.encode(int("ffff", 16)))
+
+print(':'.join(worst_case))
+print(len(':'.join(worst_case)))
+# print(len(bs.encode(li)))
